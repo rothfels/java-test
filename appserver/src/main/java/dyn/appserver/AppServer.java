635f10b8d779e9ef7f4983251a86af9fd44228e8
John Rothfels <john@dynasty.com>
2021-04-13 11:16:16 -0700

    add helper

--- appserver/src/main/java/dyn/appserver/AppServer.java
+++ appserver/src/main/java/dyn/appserver/AppServer.java
@@ -3,5 +3,6 @@ package dyn.appserver;
 public class AppServer {
   public static void main(String[] args) {
     System.out.println("hello AppServer");
+    Helper.doHelper();
   }
 }
---
+++ appserver/src/main/java/dyn/appserver/Helper.java
@@ -0,0 +1,7 @@
+package dyn.appserver;
+
+public class Helper {
+  public static void doHelper() {
+    System.out.println("helper");
+  }
+}
